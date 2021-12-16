f591daf7370bb9f224bbee33d63fe73ce22394f6
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-16 13:02:13 +0200

    footer design

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,7 +1,56 @@
+import styles from "../styles/Footer.module.css";
+import Image from "next/image";
+
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <Image src="/img/bg.png" layout="fill" alt="" objectFit="cover" />
+            </div>
+            <div className={styles.item}>
+                <div className={styles.card}>
+                    <h2 className={styles.motto}>
+                        WELL BAKED SLICE OF PIZZA!
+                    </h2>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>
+                        FIND OUR RESTAURANTS
+                    </h1>
+                    <p className={styles.text}>
+                        1654 R. Don road #304.
+                        <br />New York , 85022
+                        <br /> +1 23 456
+                    </p>
+                    <p className={styles.text}>
+                        1256 K. Laquite road #235.
+                        <br />New York , 85022
+                        <br /> +1 23 456
+                    </p>
+                    <p className={styles.text}>
+                        1654 E. Erwin road #104.
+                        <br />New York , 85022
+                        <br /> +1 23 456
+                    </p>
+                    <p className={styles.text}>
+                        1614 W. Carrol st #125.
+                        <br />New York , 85022
+                        <br /> +1 23 456
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>WORKING HOURS</h1>
+                    <p className={styles.text}>
+                        MONDAY UNTIL FRIDAY
+                        <br />9:00 - 22:00
+                    </p>
+                    <p className={styles.text}>
+                        SATURDAY - SUNDAY
+                        <br />12:00 - 24:00
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- client/components/PizzaCard.js
+++ client/components/PizzaCard.js
@@ -1,10 +1,14 @@
 import styles from "../styles/PizzaCard.module.css";
+import Image from "next/image";
 
 
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-            pizza
+            <Image src="/img/pizza.png" alt="" width="500" height="500"/>
+            <h1 className={styles.title}>Peperoni</h1>
+            <span className={styles.price}>$19.50</span>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, odio.</p>
         </div>
     )
 }
