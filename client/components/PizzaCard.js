6768b0e29230d6738b16f0a6cfe0a8f6828ff500
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-16 12:21:30 +0200

    work on pizza list and pizza card

---
+++ client/components/PizzaCard.js
@@ -0,0 +1,12 @@
+import styles from "../styles/PizzaCard.module.css";
+
+
+const PizzaCard = () => {
+    return (
+        <div className={styles.container}>
+            pizza
+        </div>
+    )
+}
+
+export default PizzaCard
