800f08977d94540b6d4e0eb72f7969f0dbb61bdf
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-16 13:30:47 +0200

    styling single product page

--- client/pages/product/[id].js
+++ client/pages/product/[id].js
@@ -45,6 +45,29 @@ const Product = () => {
                         <span className={styles.number}>Large</span>
                     </div>
                 </div>
+                <h3 className={styles.choose}>Choose additional ingredients</h3>
+                <div className={styles.ingredients}>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="double" name="double" className={styles.checkbox} />
+                        <label htmlFor="double">Double ingredients</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="cheese" name="cheese" className={styles.checkbox} />
+                        <label htmlFor="cheese">Extra cheese</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="garlic" name="garlic" className={styles.checkbox} />
+                        <label htmlFor="garlic">Garlic sauce</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="spicy" name="spicy" className={styles.checkbox} />
+                        <label htmlFor="spicy">Spicy sauce</label>
+                    </div>
+                </div>
+                <div className={styles.add}>
+                    <input type="number" defaultValue={1} className={styles.quantity} />
+                    <button className={styles.button}>Add to Cart</button>
+                </div>
             </div>
         </div>
     )
